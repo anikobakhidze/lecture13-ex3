4f09f0d2ac5f2994e8dc44e85c3d2f40b37e5de6
anikobakhidze <kobakhidzeana23@gmail.com>
2022-12-21 16:24:53 +0400

    add js file

---
+++ index.js
@@ -0,0 +1,5 @@
+let arr=['h' , 'H' , 'P', 'p'];
+let upperCase=arr.filter(el => el===el.toUpperCase());
+console.log(upperCase);
+let lowerCase=arr.filter(el => el===el.toLowerCase());
+console.log(lowerCase);
